cc72992215f22d1a2935be06cc1cde44ba8f9c53
rajendraxD <rajendraxd1@gmail.com>
2025-07-22 14:54:54 +0530

    done some changes

--- src/app/shared/reusableComponent/footer/footer.ts
+++ src/app/shared/reusableComponent/footer/footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { shardModule } from '../../shared';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [shardModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
