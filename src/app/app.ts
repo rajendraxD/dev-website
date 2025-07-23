19232018e25e14bfaf579d99cad91baa4c84b022
rajendraxD <rajendraxd1@gmail.com>
2025-07-23 20:29:47 +0530

    done some changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Dashboard } from "./pages/dashboard/dashboard";
 import { Footer } from "./shared/reusableComponent/footer/footer";
 import { Header } from './shared/reusableComponent/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard,Header, Footer],
+  imports: [RouterOutlet,Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
